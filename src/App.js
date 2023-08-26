dfb685684ae28c13aadddaf72634d72a71ecf20e
Matthew Mata <matthewmata1030@gmail.com>
2023-08-25 17:24:30 -0700

    created Table component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
+
+import Table from "./components/Table";
+
+import MARKETDATA from "./data/props.json";
+import LINEDATA from "./data/alternates.json";
+
+import createTableData from "./utils/createTableData";
+
 const App = () => {
+  const [markets, setMarkets] = useState(MARKETDATA);
+  const [lines, setLines] = useState(LINEDATA);
+  const [tableData, setTableData] = useState(null);
+  
+  useEffect(() => {
+    const data = createTableData(markets, lines);
+    setTableData(data);
+  }, [setTableData]);
 
   return (
     <div>
-      test
+      <Table tableData={tableData} setTableData={setTableData} />
     </div>
   );
 };
---
+++ src/components/Table.js
@@ -0,0 +1,74 @@
+import Override from "./Override";
+import checkMarketSuspension from "../utils/checkMarketSuspension";
+
+const Table = ({ tableData, setTableData }) => {
+  if (tableData === null) return null;
+  const headings = [
+    "Player Name",
+    "Player ID",
+    "Team ID",
+    "Team Nickname",
+    "Team Abbr.",
+    "Stat Type",
+    "Stat Type ID",
+    "Position",
+    "Market Suspended",
+    "Overide Suspension",
+    "Line",
+    "Low Line",
+    "High Line",
+  ];
+
+  return (
+    <table>
+      <tr>
+        {headings.map((heading, i) => (
+          <th key={i}>{heading}</th>
+        ))}
+      </tr>
+      {tableData.map(
+        (
+          {
+            playerName,
+            playerId,
+            teamId,
+            teamNickname,
+            teamAbbr,
+            statType,
+            statTypeId,
+            position,
+            marketSuspended,
+            line,
+            override,
+            ...data
+          },
+          i
+        ) => (
+          <tr key={i}>
+            <td>{playerName}</td>
+            <td>{playerId}</td>
+            <td>{teamId}</td>
+            <td>{teamNickname}</td>
+            <td>{teamAbbr}</td>
+            <td>{statType}</td>
+            <td>{statTypeId}</td>
+            <td>{position}</td>
+            <td>
+              {JSON.stringify(
+                checkMarketSuspension(line, marketSuspended, override, data)
+              )}
+            </td>
+            <td>
+              <Override setTableData={setTableData} tableData={tableData} index={i}/>
+            </td>
+            <td>{line}</td>
+            <td>{data.lineData ? data.lineData.min : "n/a"}</td>
+            <td>{data.lineData ? data.lineData.max : "n/a"}</td>
+          </tr>
+        )
+      )}
+    </table>
+  );
+};
+
+export default Table;
